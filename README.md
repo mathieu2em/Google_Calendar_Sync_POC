4735fd263c7a4d6a2b1ffd8277f0da9ebdcdb949
mathieu perron <m.perron@t-b.ca>
2023-10-25 17:48:59 -0400

    project first draft

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Google_Calendar_Sync_POC
 a proof of concept about google calendar and syncing capabilities using Express.js
+
+## installing expressjs
+```
+npm install express googleapis express-session
+```
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const {google} = require('googleapis');
+const session = require('express-session');
+
+const app = express();
+const PORT = 3000;
+const OAuth2 = google.auth.OAuth2;
+
+// Use express-session for handling sessions
+app.use(session({
+    secret: 'your_secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const oauth2Client = new OAuth2(
+    'YOUR_CLIENT_ID',
+    'YOUR_CLIENT_SECRET',
+    'http://localhost:3000/auth/callback'
+);
+
+// Setup the Google Calendar API
+const calendar = google.calendar({version: 'v3', auth: oauth2Client});
+
+app.get('/', (req, res) => {
+    if (!req.session.tokens) {
+        return res.redirect('/auth');
+    }
+    oauth2Client.setCredentials(req.session.tokens);
+    calendar.events.list({
+        calendarId: 'primary',
+    }, (err, response) => {
+        if (err) return res.status(500).send(err);
+        res.send(response.data.items);
+    });
+});
+
+app.get('/auth', (req, res) => {
+    const url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/calendar.events'
+    });
+    res.redirect(url);
+});
+
+app.get('/auth/callback', (req, res) => {
+    const code = req.query.code;
+    oauth2Client.getToken(code, (err, tokens) => {
+        if (err) {
+            console.error('Error authenticating', err);
+            return res.status(500).send(err);
+        }
+        req.session.tokens = tokens;
+        res.redirect('/');
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}`);
+});
